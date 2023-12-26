9e06c7b76be711dcefa01f7e63071849c2edf783
Kaushik Kunal Singh <kunalsingh747@gmail.com>
2023-12-26 08:49:59 -0800

    added sprite

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,26 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML template");
+    // shape example
     sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Yellow);
+    shape.setFillColor(sf::Color::Blue);
 
+    // text example
     sf::Font font;
     font.loadFromFile("res/fonts/ComicSansMS3.ttf");
     sf::Text text("Hello World", font, 15);
-    text.setFillColor(sf::Color::Blue);
+    text.setFillColor(sf::Color::White);
 
+    // image example
+    sf::Texture texture;
+    texture.loadFromFile("res/images/duck.png");
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.scale(0.25, 0.25);
+    // sprite.setPosition(100, 100);
+
+
+    // main loop
     while (window.isOpen())
     {
         sf::Event event;
@@ -22,6 +34,7 @@ int main()
 
         window.clear();
         window.draw(shape);
+        window.draw(sprite);
         window.draw(text);
         window.display();
     }
